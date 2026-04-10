9e52135f20e8ebc7e39a3742cfe61666841e9f21
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-04-10 13:49:57 -0300

    ADD v2

---
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using GestaoDeEquipamentos.ConsoleApp.Apresentacao;
+
+TelaEquipamento telaEquipamento = new TelaEquipamento();
+
+while (true)
+{
+    string? opcaoMenu = telaEquipamento.ObterEscolhaMenuPrincipal();
+
+    if (opcaoMenu == "S")
+    {
+        Console.Clear();
+        break;
+    }
+
+    if (opcaoMenu == "1")
+        telaEquipamento.Cadastrar();
+
+    else if (opcaoMenu == "2")
+        telaEquipamento.Editar();
+
+    else if (opcaoMenu == "3")
+        telaEquipamento.Excluir();
+
+    else if (opcaoMenu == "4")
+        telaEquipamento.VisualizarTodos();
+}
--- GestãoDeEquipamentos/obj/Debug/net10.0/GestãoDeEquipamentos.AssemblyInfo.cs
+++ GestãoDeEquipamentos/obj/Debug/net10.0/GestãoDeEquipamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestãoDeEquipamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a747df68c51a7374598ca8b556d6ca6de98832ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestãoDeEquipamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestãoDeEquipamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
