3f8bbff4b719dcb096271ccc4103f7eeb3544e8a
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-09-20 00:34:45 -0300

    ADD atualização da versão informacional do assembly e ajuste no cache de entradas

--- GestaoDeEquipamentos.ConsoleApp/obj/Debug/net10.0/GestaoDeEquipamentos.ConsoleApp.AssemblyInfo.cs
+++ GestaoDeEquipamentos.ConsoleApp/obj/Debug/net10.0/GestaoDeEquipamentos.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6433ddd5448e3d253fbf49b7ae9b5ea08708ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+170d91ee12c8ffa80f2bd001a6336c5a5db7e6c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
